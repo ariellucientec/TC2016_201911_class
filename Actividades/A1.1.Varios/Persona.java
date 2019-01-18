815d3b1255469c53d3937301b6605865a56579b3
ariellucientec <ariel.garcia@itesm.mx>
2019-01-18 13:42:03 -0600

    second class: classes and objects

---
+++ Actividades/A1.1.Varios/Persona.java
@@ -0,0 +1,18 @@
+public class Persona
+{
+    //attributes
+    private int age;
+    private String name;
+    private char gender;
+
+    //methods
+    public String getName()
+    {
+        return name;
+    }
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+}
---
+++ Actividades/A1.1.Varios/Test.java
@@ -0,0 +1,9 @@
+public class Test
+{
+    public static void main(String[]args)
+    {
+        Persona ariel = new Persona();
+        ariel.setName("ariel");
+        System.out.println(ariel.getName());
+    }
+}
